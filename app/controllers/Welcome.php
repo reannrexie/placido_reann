b4e53677e710e51ca2b56192d82226789a15cb94
Reann Rexie <reannplacido2@gmail.com>
2025-09-10 15:44:47 +0800

    First commit

---
+++ app/controllers/Welcome.php
@@ -0,0 +1,16 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+class Welcome extends Controller {
+	public function index() {
+		$this->call->view('welcome_page');
+	}
+	public function Profile($fname, $lname) {
+
+			$data['lname'] = $lname;
+			$data['fname'] = $fname;
+
+		$this->call->view('profile', $data);
+	}
+}
+?>
