e50140dcab236aa9186008455e404d10c8b5bb84
Reann Rexie <reannplacido2@gmail.com>
2025-09-30 19:28:24 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12799631',
-    'password'	=> 'y1eiwkjhCw',
-    'database'	=> 'sql12799631',
+    'username'	=> 'sql12800717',
+    'password'	=> '9aAJ4Mveps',
+    'database'	=> 'sql12800717',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
